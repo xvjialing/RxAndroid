f0e9592b463c4b3498ba3313742c521bf8e80b08
xvjialing <xvjialing@outlook.com>
2017-10-29 17:31:46 +0800

    commit

---
+++ app/src/main/java/com/lytech/xvjialing/rxjava/ErrorHandlingActivity.java
@@ -0,0 +1,13 @@
+package com.lytech.xvjialing.rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ErrorHandlingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_handling);
+    }
+}
